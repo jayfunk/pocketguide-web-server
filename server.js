d7b91e6a5391e2cd2a555ebd15fce34ac8d49293
Andrew Hutchings <andrew.hutchings@gmail.com>
2016-03-24 11:04:16 +0000

    Use standard

--- server.js
+++ server.js
@@ -1,94 +1,13 @@
-require('dotenv').config({ silent: true });
+require('dotenv').config({ silent: true })
 
-const express = require('express');
-const google = require('googleapis');
-const request = require('request');
-const csv = require('csv');
-const camelCase = require('lodash/camelCase');
+const express = require('express')
+const events = require('./lib/events')
 
-const app = express();
+const app = express()
 
-app.get('/api/events', function (req, res, next) {
-  const jwtClientEmail = process.env.GOOGLE_JWT_CLIENT_EMAIL;
-  const pemPath = null;
-  const pemContents = process.env.GOOGLE_JWT_PRIVATE_KEY.split('\\n').join('\n');
-  const scopes = [
-    'https://www.googleapis.com/auth/drive.metadata.readonly',
-    'https://www.googleapis.com/auth/drive.readonly',
-  ];
-  const jwtUserEmail = null;
-  const jwtClient = new google.auth.JWT(
-    jwtClientEmail,
-    pemPath,
-    pemContents,
-    scopes,
-    jwtUserEmail
-  )
+app.get('/api/events', events)
 
-  jwtClient.authorize(function (error, tokens) {
-    if (error) {
-      console.error('Error authorizing with Google', error);
-      res.status(500);
-      res.send({message: error.message});
-      return;
-    }
-
-    const drive = google.drive({ version: 'v2' });
-    const params = {
-      auth: jwtClient,
-      fileId: process.env.GOOGLE_FILE_ID,
-    };
-
-    drive.files.get(params, function (error, file, response) {
-      if (error) {
-        console.error('Error retrieving events from Google Drive', error);
-        res.status(500);
-        res.send({message: error.message});
-        return;
-      }
-
-      var url;
-     var requestSettings = {
-       method: 'GET',
-       headers : {
-         Authorization: 'Bearer ' + jwtClient.credentials.access_token,
-       },
-       encoding: null, // Does not try to parse file contents as UTF-8
-     };
-
-     url = file.exportLinks['text/csv'];
-     if (!url) {
-       throw 'No download url was returned by the Google Drive API';
-     }
-
-     requestSettings.url = url;
-     request(requestSettings, function (err, response, body) {
-       if (err) {
-         console.error('Error downloading file from Google Drive', error);
-         res.status(500);
-         res.send({message: error.message});
-         return;
-       }
-       // body is a buffer with the file's contents
-       csv.parse(body.toString(), {columns: true, skip_empty_lines: true}, function (error, rows) {
-         res.send(rows.map(function (row) {
-           return Object.keys(row).reduce(function (normalizedRow, key) {
-            normalizedRow[normalizeFieldName(key)] = row[key];
-            return normalizedRow;
-           }, {});
-         }));
-       });
-     });
-    });
-  });
-});
-
-function normalizeFieldName(fieldName) {
-  // TODO: Replace section in trailing parens
-  return camelCase(fieldName);
-}
-
-const port = process.env.PORT;
+const port = process.env.PORT
 const server = app.listen(port, function () {
-  console.log(`pocketguide-web-server listening on :${server.address().port}`);
-});
+  console.log(`pocketguide-web-server listening on :${server.address().port}`)
+})
